2d3e1e0715724c20b4112e0e1f6f25e754e611ff
Dennis Eghtedari <year_2000_@hotmail.com>
2024-02-14 14:46:51 +0000

    fine tuned login and sign out in navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -6,18 +6,32 @@ import Logo from "../../style/assets/logo.png";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
-import { Avatar, Skeleton } from "@mui/material";
+import { Avatar } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
 
 function Navbar() {
   const { status, data: session } = useSession();
-  const [loading, setLoading] = useState(true);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
     if (status === "authenticated") {
       toast.success("Signed In");
     } else {
       toast.warning("Signed Out");
-      setLoading(false);
     }
   }, [status]);
 
@@ -44,9 +58,6 @@ function Navbar() {
           <Link className="nav__item--link" href="/">
             About Us
           </Link>
-          {status === "authenticated" ? (
-            <h1 onClick={() => signOut()}>hello</h1>
-          ) : null}
 
           <Link className="nav__item--link" href="/">
             Contact
@@ -55,6 +66,7 @@ function Navbar() {
           {status === "authenticated" ? (
             <Link href="/">
               <Avatar
+                onClick={handleClick}
                 alt={session?.user.name.slice(0, 2)}
                 src={session?.user.image}
                 sx={{ width: 35, height: 35 }}
@@ -69,6 +81,62 @@ function Navbar() {
           )}
         </div>
       </div>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&::before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <MenuItem>
+          <ListItemIcon>
+            <AccountCircleIcon fontSize="small" onClick={handleClose} />
+          </ListItemIcon>
+          Dashboard
+        </MenuItem>
+        <Divider />
+
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={ () => signOut()}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
     </nav>
   );
 }
