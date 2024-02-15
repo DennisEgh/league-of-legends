16f32544b8a3f0d556e3a3e522b27326dcf7aa4f
Dennis Eghtedari <year_2000_@hotmail.com>
2024-02-15 20:40:33 +0000

    added carousel

--- components/Partners/Partners.jsx
+++ components/Partners/Partners.jsx
@@ -1,13 +1,69 @@
+"use client"
+import Image from "next/image";
 import "./Partners.css";
+import img1 from "../../style/assets/account.svg"
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
 
-import React from "react";
 
 const Partners = () => {
+    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
+
   return (
     <section id="partners">
       <h1 className="partners__title">
         Banks that trust, and offer our services
       </h1>
+      <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        <div className="embla__slide">
+            1
+            <Image
+            width={24}
+            height={24}
+            alt="leim"
+            src={img1}
+            />
+        </div>
+        <div className="embla__slide">
+            2
+        <Image
+            width={24}
+            height={24}
+            alt="leim"
+            src={img1}
+            />
+        </div>
+        <div className="embla__slide">
+            3
+        <Image
+            width={24}
+            height={24}
+            alt="leim"
+            src={img1}
+            />
+        </div>
+        <div className="embla__slide">
+            4
+        <Image
+            width={24}
+            height={24}
+            alt="leim"
+            src={img1}
+            />
+        </div>
+        <div className="embla__slide">
+            5
+        <Image
+            width={24}
+            height={24}
+            alt="leim"
+            src={img1}
+            />
+        </div>
+      </div>
+    </div>
+
     </section>
   );
 };
