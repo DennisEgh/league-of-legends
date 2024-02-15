c4354f5ec84c89ce856d3553ca6caafd97337f46
Dennis Eghtedari <year_2000_@hotmail.com>
2024-02-15 20:54:24 +0000

    save

--- components/Partners/Partners.jsx
+++ components/Partners/Partners.jsx
@@ -1,13 +1,17 @@
-"use client"
+"use client";
 import Image from "next/image";
 import "./Partners.css";
-import img1 from "../../style/assets/account.svg"
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-
+import img1 from "../../style/assets/account.svg";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import SEB from "../../style/assets/SEB.svg";
+import Danskebank from "../../style/assets/Danskebank.svg";
+import Nordea from "../../style/assets/Nordea.svg";
+import Swedbank from "../../style/assets/Swedbank.svg";
+import Handelsbanken from "../../style/assets/Handelsbanken.svg"
 
 const Partners = () => {
-    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()]);
 
   return (
     <section id="partners">
@@ -15,55 +19,63 @@ const Partners = () => {
         Banks that trust, and offer our services
       </h1>
       <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">
-            1
+        <div className="embla__container">
+          <div className="embla__slide">
             <Image
-            width={24}
-            height={24}
-            alt="leim"
-            src={img1}
+              className="bank"
+              loading="lazy"
+              width={100}
+              height={50}
+              alt="leim"
+              src={Danskebank}
             />
-        </div>
-        <div className="embla__slide">
-            2
-        <Image
-            width={24}
-            height={24}
-            alt="leim"
-            src={img1}
+          </div>
+          <div className="embla__slide">
+            <Image
+              className="bank"
+              loading="lazy"
+              width={100}
+              height={50}
+              alt="leim"
+              src={Swedbank}
             />
-        </div>
-        <div className="embla__slide">
-            3
-        <Image
-            width={24}
-            height={24}
-            alt="leim"
-            src={img1}
+          </div>
+          <div className="embla__slide">
+            <Image
+              className="bank"
+              loading="lazy"
+              width={75}
+              height={75}
+              alt="leim"
+              src={SEB}
             />
-        </div>
-        <div className="embla__slide">
-            4
-        <Image
-            width={24}
-            height={24}
-            alt="leim"
-            src={img1}
+          </div>
+          <div className="embla__slide">
+            <Image
+              className="bank"
+              loading="lazy"
+              width={100}
+              height={50}
+              alt="leim"
+              src={Nordea}
             />
-        </div>
-        <div className="embla__slide">
-            5
-        <Image
-            width={24}
-            height={24}
-            alt="leim"
-            src={img1}
+        
+          
+          </div>
+          <div className="embla__slide">
+            <Image
+              className="bank"
+              loading="lazy"
+              width={100}
+              height={50}
+              alt="leim"
+              src={Handelsbanken}
             />
+        
+          
+          </div>
         </div>
       </div>
-    </div>
-
     </section>
   );
 };
