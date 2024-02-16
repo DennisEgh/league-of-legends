15da1c4aadc7220dd3ecb8edabe5fda8220f6de8
Dennis Eghtedari <year_2000_@hotmail.com>
2024-02-16 20:17:55 +0000

    sssss

--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,43 @@
 import Link from "next/link";
 import Partners from "../components/Partners/Partners";
+
 export default function Home() {
   return (
     <>
-    <section id="landing__page">
-      <div className="landing__page--container ">
-        <h1 className="landing__page--container--title fade-in">
-          Can't afford to pay your invoice this month?
-        </h1>
+      <section id="landing__page">
+        <div className="landing__page--container">
+          <h1 className="landing__page--container--title fade-in">
+            Can't afford to pay your invoice this month?
+          </h1>
 
-        <p className="landing__page--container--para fade-in-second">
-          We've got you covered.
-        </p>
-        <div className="landing__page--cta--container">
-
-        <Link className="landing__page--container--link fade-in-second" href="/">
-          <div className="landing__page--container--cta ctacolor ">
-            <p className="landing__page--container--cta--para">
-              Get Started
-            </p>
+          <p className="landing__page--container--para fade-in-second">
+            We've got you covered.
+          </p>
+          <div className="landing__page--cta--container">
+            <Link
+              className="landing__page--container--link fade-in-second"
+              href="/"
+            >
+              <div className="landing__page--container--cta ctacolor">
+                <p className="landing__page--container--cta--para">
+                  Get Started
+                </p>
+              </div>
+            </Link>
+            <Link
+              className="landing__page--container--link fade-in-second"
+              href="/"
+            >
+              <div className="landing__page--container--cta">
+                <p className="landing__page--container--cta--para">
+                  Learn More
+                </p>
+              </div>
+            </Link>
           </div>
-        </Link>
-        <Link className="landing__page--container--link fade-in-second" href="/">
-          <div className="landing__page--container--cta  ">
-            <p className="landing__page--container--cta--para ">
-              Learn More
-            </p>
-          </div>
-        </Link>
         </div>
-      </div>
-    </section>
-    <Partners />
-    
-    
+      </section>
+      <Partners />
     </>
   );
 }
